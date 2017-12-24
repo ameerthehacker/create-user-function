0acd285fbca12a95f759828e492b3e1c4383e21d
ameerthehacker <ameerjhanprof@gmail.com>
2017-12-24 19:41:05 +0530

    Add cloud function to create an user account in firestore whenever a new user logs in

--- functions/index.js
+++ functions/index.js
@@ -1,5 +1,17 @@
 const functions = require('firebase-functions');
+const admin = require('firebase-admin');
 
-exports.helloWorld = functions.https.onRequest((request, response) => {
- response.send("Hello from Firebase!");
+// Initialize firebase admin sdk
+admin.initializeApp(functions.config().firebase);
+
+exports.createUserAccount = functions.auth.user().onCreate((event) => {
+    const user = event.data;
+    const uid = user.uid;
+    let newUser = {};
+    newUser.email = user.email;
+    newUser.displayName = user.displayName;
+    // If user has no dp make a default one from the firebase storage
+    newUser.photoURL = user.photoURL || "https://firebasestorage.googleapis.com/v0/b/letschat-bbbfd.appspot.com/o/default-dp.png?alt=media&token=2508f65b-e1ff-4782-a2c3-7d4c9b04e3d9";
+
+    return admin.firestore().doc(`/users/${uid}`).set(newUser);
 });
