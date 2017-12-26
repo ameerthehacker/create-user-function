5f3e70d4b87f3178ea7746f99c5a07d7e14cad81
ameerthehacker <ameerjhanprof@gmail.com>
2017-12-26 19:53:04 +0530

    Add undefined checks and add a default value for them

--- functions/index.js
+++ functions/index.js
@@ -8,11 +8,11 @@ exports.createUserAccount = functions.auth.user().onCreate((event) => {
     const user = event.data;
     const uid = user.uid;
     let newUser = {};
-    newUser.email = user.email;
-    newUser.displayName = user.displayName;
+    newUser.email = user.email || '';
+    newUser.displayName = user.displayName || 'LetsChat User';
     // If user has no dp make a default one from the firebase storage
     newUser.photoURL = user.photoURL || "https://firebasestorage.googleapis.com/v0/b/letschat-bbbfd.appspot.com/o/default-dp.png?alt=media&token=2508f65b-e1ff-4782-a2c3-7d4c9b04e3d9";
-    newUser.status = "Hey I'm in letschat";
+    newUser.status = 'Let us chat!';
 
     return admin.firestore().doc(`/users/${uid}`).set(newUser);
 });
